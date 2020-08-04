9971f2cd00cb2825906eac191a7c33df248a2ac1
brugobi <brgobbi@hotmail.com>
2020-08-04 17:05:07 -0400

    add a has_many to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attendees, through: :attendances, source: :attendee
+  has_many :attendances, foreign_key: 'attendee_id', class_name: 'Attendance'
 end
