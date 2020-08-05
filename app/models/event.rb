869e8056deeb32ac6657d475b406e9d8bd9c1976
emiliocm9 <emilio.contreras97@gmail.com>
2020-08-05 12:37:44 -0500

    CHECK TSE

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :attendees, through: :attendances, source: :attendee
-  has_many :attendances, foreign_key: 'attendee_id', class_name: 'Attendance'
+  has_many :attendees, through: :attendances, source: :attendee, class_name: 'User'
+  has_many :attendances, foreign_key: 'attended_event_id', class_name: 'Attendance'
 end
